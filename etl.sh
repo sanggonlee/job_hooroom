b577ab2a516f7f325c247348ee68a3c982f0f514
sanggonlee <lee.sg16@gmail.com>
2020-09-02 21:29:31 -0400

    change log file organization

--- etl.sh
+++ etl.sh
@@ -2,4 +2,4 @@
 . env.sh;
 datetime=$(date '+%Y%m%d%H%M%S')
 # TODO: Make it cross environment
-cd ~/prjs/job_hooroom && /usr/local/opt/python/libexec/bin/python -m etl >~/prjs/job_hooroom/cronlog/log/${datetime}.log 2>~/prjs/job_hooroom/cronlog/err/${datetime}.log
+cd ~/prjs/job_hooroom && /usr/local/opt/python/libexec/bin/python -m etl >~/prjs/job_hooroom/cronlog/${datetime}.log 2>~/prjs/job_hooroom/cronlog/${datetime}.err.log
